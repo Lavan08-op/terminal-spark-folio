a2f447181d0c4654cc7016f437a75963757e5373
lavan2133 <lavanyasaini615@gmail.com>
2026-07-20 00:39:20 +0530

    Build ready for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import nitro from "nitro";
 
 export default defineConfig({
   plugins: [
     tanstackStart({
       server: { entry: "src/server.ts" },
     }),
+    nitro(),
     react(),
     tailwindcss(),
     tsConfigPaths(),
